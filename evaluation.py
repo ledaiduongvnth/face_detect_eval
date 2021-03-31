6f1ae871dbfb604e3a985242c1529ff79b638456
d <ledaiduongvnth@gmail.com>
2021-03-31 10:35:39 +0700

    Done t_retina_ncnn_resnet50

--- evaluation.py
+++ evaluation.py
@@ -283,7 +283,7 @@ def evaluation(pred, gt_path, iou_thresh=0.5):
 
 if __name__ == '__main__':
     ground_truth = "ground_truth/"
-    prediction = "t_retina_ncnn_mobilenetv2/prediction"
+    prediction = "t_retina_pytorch_resnet50/prediction"
     evaluation(prediction, ground_truth)
 
 
