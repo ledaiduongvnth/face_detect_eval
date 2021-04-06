82975904848f43f88366b2b2e3743b6a31a4bf28
d <ledaiduongvnth@gmail.com>
2021-04-06 16:27:56 +0700

    origin size

--- t_retina_onnxruntime_pytorch_resnet50/FaceDetector.h
+++ t_retina_onnxruntime_pytorch_resnet50/FaceDetector.h
@@ -5,12 +5,7 @@
 #ifndef FACE_DETECTOR_H
 #define FACE_DETECTOR_H
 
-#include <opencv2/opencv.hpp>
-#include <string>
-#include <stack>
-#include <chrono>
 
-using namespace std::chrono;
 
 struct Point{
     float _x;
@@ -30,30 +25,5 @@ struct box{
     float cy;
     float sx;
     float sy;
-};
-
-class Detector
-{
-public:
-    Detector();
-    ~Detector();
-    inline void Release();
-
-
-    void nms(std::vector<bbox> &input_boxes, float NMS_THRESH);
-
-    void Detect(std::vector<bbox>& boxes,  std::vector<std::vector<float>> results, int inh, int inw, std::vector<box>& anchor);
-
-    void create_anchor(std::vector<box> &anchor, int w, int h);
-
-    void create_anchor_retinaface(std::vector<box> &anchor, int w, int h);
-
-    static inline bool cmp(bbox a, bbox b);
-
-public:
-    float _nms;
-    float _threshold;
-    float _mean_val[3];
-
 };
 #endif //
--- t_retina_onnxruntime_pytorch_resnet50/main.cpp
+++ t_retina_onnxruntime_pytorch_resnet50/main.cpp
@@ -5,11 +5,12 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <chrono>
-#include <iostream>
 #include <string>
 #include <vector>
 #include <onnxruntime_cxx_api.h>
-#include "FaceDetector.h"
+#include <opencv2/opencv.hpp>
+
+float _nms(0.4), _threshold(0.99);
 
 std::vector<std::string> readLines(const std::string &filename) {
     std::ifstream ifs(filename);
@@ -25,10 +26,149 @@ std::vector<std::string> readLines(const std::string &filename) {
     return lines;
 }
 
+bool cmp(bbox a, bbox b) {
+    if (a.s > b.s)
+        return true;
+    return false;
+}
+
+void nms(std::vector<bbox> &input_boxes, float NMS_THRESH)
+{
+    std::vector<float>vArea(input_boxes.size());
+    for (int i = 0; i < int(input_boxes.size()); ++i)
+    {
+        vArea[i] = (input_boxes.at(i).x2 - input_boxes.at(i).x1 + 1)
+                   * (input_boxes.at(i).y2 - input_boxes.at(i).y1 + 1);
+    }
+    for (int i = 0; i < int(input_boxes.size()); ++i)
+    {
+        for (int j = i + 1; j < int(input_boxes.size());)
+        {
+            float xx1 = std::max(input_boxes[i].x1, input_boxes[j].x1);
+            float yy1 = std::max(input_boxes[i].y1, input_boxes[j].y1);
+            float xx2 = std::min(input_boxes[i].x2, input_boxes[j].x2);
+            float yy2 = std::min(input_boxes[i].y2, input_boxes[j].y2);
+            float w = std::max(float(0), xx2 - xx1 + 1);
+            float   h = std::max(float(0), yy2 - yy1 + 1);
+            float   inter = w * h;
+            float ovr = inter / (vArea[i] + vArea[j] - inter);
+            if (ovr >= NMS_THRESH)
+            {
+                input_boxes.erase(input_boxes.begin() + j);
+                vArea.erase(vArea.begin() + j);
+            }
+            else
+            {
+                j++;
+            }
+        }
+    }
+}
+
+
+void Detect(std::vector<bbox>& boxes,  std::vector<std::vector<float>> results, int inh, int inw, std::vector<box>& anchor)
+{
+    std::vector<bbox > total_box;
+    std::vector<float> loc = results[0];
+    std::vector<float> landms = results[2];
+    std::vector<float> score = results[1];
+    int indexLoc=0;
+    int indexLm=0;
+    int indexsco=0;
+
+    // #pragma omp parallel for num_threads(2)
+    for (int i = 0; i < anchor.size(); ++i)
+    {
+        if (score[indexsco+1] > _threshold)
+        {
+            box tmp = anchor[i];
+            box tmp1;
+            bbox result;
+            // loc and conf
+            tmp1.cx = tmp.cx + loc[indexLoc] * 0.1 * tmp.sx;
+            tmp1.cy = tmp.cy + loc[indexLoc+1] * 0.1 * tmp.sy;
+            tmp1.sx = tmp.sx * exp(loc[indexLoc+2] * 0.2);
+            tmp1.sy = tmp.sy * exp(loc[indexLoc+3] * 0.2);
+            result.x1 = (tmp1.cx - tmp1.sx/2) * inw;
+            if (result.x1<0)
+                result.x1 = 0;
+            result.y1 = (tmp1.cy - tmp1.sy/2) * inh;
+            if (result.y1<0)
+                result.y1 = 0;
+            result.x2 = (tmp1.cx + tmp1.sx/2) * inw;
+            if (result.x2>inw)
+                result.x2 = inw;
+            result.y2 = (tmp1.cy + tmp1.sy/2)* inh;
+            if (result.y2>inh)
+                result.y2 = inh;
+            result.s = score[indexsco+1];
+            // landmark
+            for (int j = 0; j < 5; ++j)
+            {
+                result.point[j]._x =( tmp.cx + landms[indexLm + (j<<1)] * 0.1 * tmp.sx ) * inw;
+                result.point[j]._y =( tmp.cy + landms[indexLm + (j<<1) + 1] * 0.1 * tmp.sy ) * inh;
+            }
+
+            total_box.push_back(result);
+        }
+        indexLoc += 4;
+        indexsco += 2;
+        indexLm += 10;
+    }
+
+    std::sort(total_box.begin(), total_box.end(), cmp);
+    nms(total_box, _nms);
+//    printf("Total box %d\n", indexsco);
+
+    for (int j = 0; j < total_box.size(); ++j)
+    {
+        boxes.push_back(total_box[j]);
+    }
+}
+
+
+void create_anchor_retinaface(std::vector<box> &anchor, int w, int h)
+{
+//    anchor.reserve(num_boxes);
+    anchor.clear();
+    std::vector<std::vector<int> > feature_map(3), min_sizes(3);
+    float steps[] = {8, 16, 32};
+    for (int i = 0; i < feature_map.size(); ++i) {
+        feature_map[i].push_back(ceil(h/steps[i]));
+        feature_map[i].push_back(ceil(w/steps[i]));
+    }
+    std::vector<int> minsize1 = {16, 32};
+    min_sizes[0] = minsize1;
+    std::vector<int> minsize2 = {64, 128};
+    min_sizes[1] = minsize2;
+    std::vector<int> minsize3 = {256, 512};
+    min_sizes[2] = minsize3;
+
+    for (int k = 0; k < feature_map.size(); ++k)
+    {
+        std::vector<int> min_size = min_sizes[k];
+        for (int i = 0; i < feature_map[k][0]; ++i)
+        {
+            for (int j = 0; j < feature_map[k][1]; ++j)
+            {
+                for (int l = 0; l < min_size.size(); ++l)
+                {
+                    float s_kx = min_size[l]*1.0/w;
+                    float s_ky = min_size[l]*1.0/h;
+                    float cx = (j + 0.5) * steps[k]/w;
+                    float cy = (i + 0.5) * steps[k]/h;
+                    box axil = {cx, cy, s_kx, s_ky};
+                    anchor.push_back(axil);
+                }
+            }
+        }
+
+    }
+
+}
 
 
 int main() {
-    Detector detector;
     std::string modelFilepath{"/mnt/hdd/PycharmProjects/Pytorch_Retinaface/FaceDetector.onnx"};
     Ort::Env env(OrtLoggingLevel::ORT_LOGGING_LEVEL_WARNING);
     Ort::SessionOptions sessionOptions;
@@ -77,7 +217,7 @@ int main() {
 
         /////////////////////////////////////////////////////////////////////////
         std::vector<box> anchor;
-        detector.create_anchor_retinaface(anchor, img.cols, img.rows);
+        create_anchor_retinaface(anchor, img.cols, img.rows);
         int anchorsSize = anchor.size();
         std::vector<int> outputDims{anchorsSize*4, anchorsSize*2, anchorsSize*10};
         cv::Mat originImage= img.clone();
@@ -101,22 +241,22 @@ int main() {
             std::vector<float> outputi = std::vector<float>(ort_outputs[l].GetTensorMutableData<float>(), ort_outputs[l].GetTensorMutableData<float>() + outputDims[l]);
             results.emplace_back(outputi);
         }
-        detector.Detect(result, results, img.rows, img.cols, anchor);
+        Detect(result, results, img.rows, img.cols, anchor);
 
-//        for (int j = 0; j < result.size(); ++j) {
-//            cv::Rect rect(result[j].x1/scale, result[j].y1/scale, result[j].x2/scale - result[j].x1/scale, result[j].y2/scale - result[j].y1/scale);
-//            cv::rectangle(originImage, rect, cv::Scalar(0, 0, 255), 1, 8, 0);
-//            char test[80];
-//            sprintf(test, "%f", result[j].s);
-//            cv::putText(originImage, test, cv::Size((result[j].x1/scale), result[j].y1/scale), cv::FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(0, 255, 255));
-//            cv::circle(originImage, cv::Point(result[j].point[0]._x / scale, result[j].point[0]._y / scale), 1, cv::Scalar(0, 0, 225), 4);
-//            cv::circle(originImage, cv::Point(result[j].point[1]._x / scale, result[j].point[1]._y / scale), 1, cv::Scalar(0, 255, 225), 4);
-//            cv::circle(originImage, cv::Point(result[j].point[2]._x / scale, result[j].point[2]._y / scale), 1, cv::Scalar(255, 0, 225), 4);
-//            cv::circle(originImage, cv::Point(result[j].point[3]._x / scale, result[j].point[3]._y / scale), 1, cv::Scalar(0, 255, 0), 4);
-//            cv::circle(originImage, cv::Point(result[j].point[4]._x / scale, result[j].point[4]._y / scale), 1, cv::Scalar(255, 0, 0), 4);
-//        }
-//        cv::imshow("img", originImage);
-//        cv::waitKey(0);
+        for (int j = 0; j < result.size(); ++j) {
+            cv::Rect rect(result[j].x1/scale, result[j].y1/scale, result[j].x2/scale - result[j].x1/scale, result[j].y2/scale - result[j].y1/scale);
+            cv::rectangle(originImage, rect, cv::Scalar(0, 0, 255), 1, 8, 0);
+            char test[80];
+            sprintf(test, "%f", result[j].s);
+            cv::putText(originImage, test, cv::Size((result[j].x1/scale), result[j].y1/scale), cv::FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(0, 255, 255));
+            cv::circle(originImage, cv::Point(result[j].point[0]._x / scale, result[j].point[0]._y / scale), 1, cv::Scalar(0, 0, 225), 4);
+            cv::circle(originImage, cv::Point(result[j].point[1]._x / scale, result[j].point[1]._y / scale), 1, cv::Scalar(0, 255, 225), 4);
+            cv::circle(originImage, cv::Point(result[j].point[2]._x / scale, result[j].point[2]._y / scale), 1, cv::Scalar(255, 0, 225), 4);
+            cv::circle(originImage, cv::Point(result[j].point[3]._x / scale, result[j].point[3]._y / scale), 1, cv::Scalar(0, 255, 0), 4);
+            cv::circle(originImage, cv::Point(result[j].point[4]._x / scale, result[j].point[4]._y / scale), 1, cv::Scalar(255, 0, 0), 4);
+        }
+        cv::imshow("img", originImage);
+        cv::waitKey(0);
 
         /////////////////////////////////////////////////////////////////////////
         std::string abs_file_name = p.filename().string();
