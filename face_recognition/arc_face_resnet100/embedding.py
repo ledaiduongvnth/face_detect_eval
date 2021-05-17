0d898846c9b405a672fa505d1e510dae78908a7d
d <ledaiduongvnth@gmail.com>
2021-05-17 12:28:02 +0700

    Update face_common.so

--- face_recognition/arc_face_resnet100/embedding.py
+++ face_recognition/arc_face_resnet100/embedding.py
@@ -11,17 +11,17 @@ import face_common as face_common
 
 class Embedding:
   def __init__(self, prefix, epoch, ctx_id=0):
-    print('loading',prefix, epoch)
-    ctx = mx.gpu(ctx_id)
-    sym, arg_params, aux_params = mx.model.load_checkpoint(prefix, epoch)
-    all_layers = sym.get_internals()
-    sym = all_layers['fc1_output']
+    # print('loading',prefix, epoch)
+    # ctx = mx.gpu(ctx_id)
+    # sym, arg_params, aux_params = mx.model.load_checkpoint(prefix, epoch)
+    # all_layers = sym.get_internals()
+    # sym = all_layers['fc1_output']
     image_size = (112,112)
     self.image_size = image_size
-    model = mx.mod.Module(symbol=sym, context=ctx, label_names = None)
-    model.bind(for_training=False, data_shapes=[('data', (2, 3, image_size[0], image_size[1]))])
-    model.set_params(arg_params, aux_params)
-    self.model = model
+    # model = mx.mod.Module(symbol=sym, context=ctx, label_names = None)
+    # model.bind(for_training=False, data_shapes=[('data', (2, 3, image_size[0], image_size[1]))])
+    # model.set_params(arg_params, aux_params)
+    # self.model = model
     src = np.array([
       [30.2946, 51.6963],
       [65.5318, 51.5014],
@@ -40,8 +40,41 @@ class Embedding:
   def get(self, rimg, landmark):
     assert landmark.shape[0]==68 or landmark.shape[0]==5
     assert landmark.shape[1]==2
-    feature1 = self.face_recognizer.Process(rimg)
-    img_flip = np.fliplr(rimg)
+    if landmark.shape[0]==68:
+      landmark5 = np.zeros( (5,2), dtype=np.float32 )
+      landmark5[0] = (landmark[36]+landmark[39])/2
+      landmark5[1] = (landmark[42]+landmark[45])/2
+      landmark5[2] = landmark[30]
+      landmark5[3] = landmark[48]
+      landmark5[4] = landmark[54]
+    else:
+      landmark5 = landmark
+
+    boxes = self.face_recognizer.Detect(rimg, False, True)
+    if len(boxes) == 1:
+      landmark5 = np.array([[int(boxes[0].landmarks[0].x), int(boxes[0].landmarks[0].y)],
+                               [int(boxes[0].landmarks[1].x), int(boxes[0].landmarks[1].y)],
+                               [int(boxes[0].landmarks[2].x), int(boxes[0].landmarks[2].y)],
+                               [int(boxes[0].landmarks[3].x), int(boxes[0].landmarks[3].y)],
+                               [int(boxes[0].landmarks[4].x), int(boxes[0].landmarks[4].y)]
+                               ])
+    else:
+      print("Cannot detect faces")
+
+    # cv2.circle(rimg, (int(landmark_vts[0][0]), int(landmark_vts[0][1])), 1, (255, 0, 0), 4)
+    # cv2.circle(rimg, (int(landmark5[0][0]), int(landmark5[0][1])), 1, (0, 0, 255), 4)
+
+    tform = trans.SimilarityTransform()
+    tform.estimate(landmark5, self.src)
+    M = tform.params[0:2,:]
+    img = cv2.warpAffine(rimg,M,(self.image_size[1],self.image_size[0]), borderValue = 0.0)
+
+    # cv2.imshow("aligned img", img)
+    # cv2.waitKey(2000)
+    # cv2.destroyAllWindows()
+
+    img_flip = np.fliplr(img)
+    feature1 = self.face_recognizer.Recognize(img)
     feature2 = self.face_recognizer.Process(img_flip)
     if len(feature1) == 512 and len(feature2) == 512:
       feature1.extend(feature2)
