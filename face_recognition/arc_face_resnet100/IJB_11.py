991943fbe5235fdc16e3534b3c617c5d26f9e421
d <ledaiduongvnth@gmail.com>
2021-05-21 08:16:25 +0700

    nothing

--- face_recognition/arc_face_resnet100/IJB_11.py
+++ face_recognition/arc_face_resnet100/IJB_11.py
@@ -36,7 +36,7 @@ parser.add_argument('--target',
                     type=str,
                     help='target, set to IJBC or IJBB')
 args = parser.parse_args()
-data_path = "/hdd/IJB"
+data_path = "/mnt/hdd/PycharmProjects/insightface/evaluation/IJB"
 
 target = args.target
 model_path = args.model_prefix
