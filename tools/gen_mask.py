84d0103fed10cfe4c7f415ae6a06640ea2f65c46
d <ledaiduongvnth@gmail.com>
2022-06-10 07:53:13 +0700

    Edit genmask.py

--- tools/gen_mask.py
+++ tools/gen_mask.py
@@ -2,53 +2,22 @@ import os
 import cv2
 from insightface.app import MaskRenderer
 from numpy import random
-from face_masker import FaceMasker
-
-import yaml
-import numpy as np
-from core.model_loader.face_alignment.FaceAlignModelLoader import FaceAlignModelLoader
-from core.model_handler.face_alignment.FaceAlignModelHandler import FaceAlignModelHandler
-from scrfd import SCRFD
 
 # TODO CHANGE PROCESS ORDER
-process_order = 1
-num_process = 2
-gpu_id = 2
+process_order = 0
+num_process = 1
+gpu_id = 0
+start_folder = 0
 
-detector = SCRFD(model_file="scrfd_500m_bnkps_shape320x320.onnx")
-detector.prepare(gpu_id)
-
-with open('config/model_conf.yaml') as f:
-    model_conf = yaml.load(f)
-
-model_path = 'models'
-scene = 'non-mask'
-model_category = 'face_alignment'
-model_name = model_conf[scene][model_category]
-
-faceAlignModelLoader = FaceAlignModelLoader(model_path, model_category, model_name)
-model, cfg = faceAlignModelLoader.load_model()
-faceAlignModelHandler = FaceAlignModelHandler(model, 'cuda:' + str(gpu_id), cfg)
-
-
-def FaceXZoo(input_img_path, origin_image, mask_img, output_img_path):
-    bboxes, kpss = detector.detect(origin_image, 0.01, input_size=(320, 320))
-    bbox = bboxes[0].astype(np.int)
-    bbox = bbox[0:4]
-    landmarks = faceAlignModelHandler.inference_on_image(origin_image, bbox)
-    is_aug = False
-    face_masker = FaceMasker(is_aug, gpu_id=gpu_id)
-    face_masker.add_mask_one(input_img_path, landmarks, mask_img, output_img_path)
-
-
-input_dataset_dir = "/media/ubuntu/DATA/vinh/face-datasets/webface/images/data/WebFace260M"
-output_dataset_dir = "/media/ubuntu/DATA/vinh/face-datasets/webface/images/data/WebFace260MMask"
+input_dataset_dir = "/root/GenMask/WebFace260M"
+output_dataset_dir = "/root/GenMask/WebFace260MMask"
 # Create output dataset dir
 if not os.path.exists(output_dataset_dir):
     os.makedirs(output_dataset_dir)
 
 dirs = os.listdir(input_dataset_dir)
 dirs = sorted(dirs)
+del dirs[:start_folder]
 num_dirs = len(dirs)
 num_images_per_task = int(num_dirs / num_process)
 job_dir = dirs[process_order * num_images_per_task: (process_order + 1) * num_images_per_task]
@@ -56,11 +25,9 @@ print("Total folder:", len(job_dir))
 
 tool = MaskRenderer()
 tool.prepare(ctx_id=gpu_id, det_size=(128, 128))
-position_style = [[0.1, 0.5, 0.9, 0.7], [0.1, 0.33, 0.9, 0.7]]
-algorithm_list = ["insightface", "facexzoo"]
+position_style = [[0.1, 0.33, 0.9, 0.7], [0.1, 0.5, 0.9, 0.7]]
 
-insightface_masks = ["mask_white", "mask_black", "mask_blue", "mask_green"]
-faceXZoo_masks = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png']
+masks = ["mask_white", "mask_black", "mask_blue", "mask_green"]
 
 # loop over each folder in the dataset
 for i_dir, image_dir in enumerate(job_dir):
@@ -70,32 +37,82 @@ for i_dir, image_dir in enumerate(job_dir):
     # Create image dir
     if not os.path.exists(real_output_image_dir):
         os.makedirs(real_output_image_dir)
+
+    num_interation = -1
+    number_files_in_folder = len(os.listdir(real_input_image_dir))
+    if number_files_in_folder <= 5:
+        num_interation = int(10/number_files_in_folder)
+
+    # need to check number of images in each folder
     files_path = [f for f in os.listdir(real_input_image_dir) if os.path.isfile(os.path.join(real_input_image_dir, f))]
     # loop over files:
     for file_path in files_path:
+        # Write origin image
+        real_input_file_path = os.path.join(real_input_image_dir, file_path)
+        real_output_file_path = os.path.join(real_output_image_dir, file_path.rstrip(".jpg") + "_" + str(-1) + ".jpg")
+        # read image from real_input_file_path, add mask and write to real_output_file_path
+        # print("Process:", real_output_file_path)
+        image = cv2.imread(real_input_file_path)
+        try:
+            cv2.imwrite(real_output_file_path, image)
+            print("Write to:", real_output_file_path)
+        except Exception as e:
+            print(e)
+
+        # Gen image
         real_input_file_path = os.path.join(real_input_image_dir, file_path)
         real_output_file_path = os.path.join(real_output_image_dir, file_path)
         # read image from real_input_file_path, add mask and write to real_output_file_path
-        print("Process:", real_input_file_path)
+        # print("Process:", real_output_file_path)
         image = cv2.imread(real_input_file_path)
-
-        algorithm_index = random.randint(len(algorithm_list))
-        algorithm = algorithm_list[algorithm_index]
-
-        if algorithm == "insightface":
-            mask_index = random.randint(len(insightface_masks))
-            mask_image_name = insightface_masks[mask_index]
-            style_index = random.randint(len(position_style))
-            position = position_style[style_index]
+        mask_index = random.randint(len(masks))
+        mask_image = masks[mask_index]
+        style_index = 0
+        position = position_style[style_index]
+        try:
             params = tool.build_params(image)
+            mask_out = tool.render_mask(image, mask_image, params, positions=position)
+            cv2.imwrite(real_output_file_path, mask_out)
+            print("Write to:", real_output_file_path)
+        except Exception as e:
+            print(e)
+
+        # Gen half mask image
+        gen_half_mask = random.randint(5) == 0
+        if gen_half_mask:
+            real_input_file_path = os.path.join(real_input_image_dir, file_path)
+            real_output_file_path = os.path.join(real_output_image_dir, file_path.rstrip(".jpg") + "_" + str(-2) + ".jpg")
+            # read image from real_input_file_path, add mask and write to real_output_file_path
+            # print("Process:", real_output_file_path)
+            image = cv2.imread(real_input_file_path)
+            mask_index = random.randint(len(masks))
+            mask_image = masks[mask_index]
+            style_index = 1
+            position = position_style[style_index]
             try:
-                mask_out = tool.render_mask(image, mask_image_name, params, positions=position)
+                params = tool.build_params(image)
+                mask_out = tool.render_mask(image, mask_image, params, positions=position)
                 cv2.imwrite(real_output_file_path, mask_out)
                 print("Write to:", real_output_file_path)
-            except:
-                print("ERROR")
+            except Exception as e:
+                print(e)
 
-        elif algorithm == "facexzoo":
-            mask_index = random.randint(len(faceXZoo_masks))
-            mask_image_name = faceXZoo_masks[mask_index]
-            FaceXZoo(real_input_file_path, image, mask_image_name, real_output_file_path)
+        # Duplicate image
+        if num_interation > 0:
+            for i in range(num_interation):
+                real_input_file_path = os.path.join(real_input_image_dir, file_path)
+                real_output_file_path = os.path.join(real_output_image_dir, file_path.rstrip(".jpg") + "_" + str(i) + ".jpg")
+                # read image from real_input_file_path, add mask and write to real_output_file_path
+                # print("Process:", real_output_file_path)
+                image = cv2.imread(real_input_file_path)
+                mask_index = random.randint(len(masks))
+                mask_image = masks[mask_index]
+                style_index = 0
+                position = position_style[style_index]
+                try:
+                    params = tool.build_params(image)
+                    mask_out = tool.render_mask(image, mask_image, params, positions=position)
+                    cv2.imwrite(real_output_file_path, mask_out)
+                    print("Write to:", real_output_file_path)
+                except Exception as e:
+                    print(e)
